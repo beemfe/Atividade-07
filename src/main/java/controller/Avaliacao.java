b79eaf9a8f1bad5f63852843c7fc3cbd215821b6
beemfe <144623179+beemfe@users.noreply.github.com>
2024-04-18 16:24:22 -0300

    Avaliacao.java

--- src/main/java/controller/Avaliacao.java
+++ src/main/java/controller/Avaliacao.java
@@ -1,5 +1,7 @@
+import java.util.List;
 public interface Avaliacao<T> {
     void adicionarAtividade(T atividade);
     T consultarAtividade(int posicao);
     void limparAtividades();
+    List<T> getAtividades();
 }
